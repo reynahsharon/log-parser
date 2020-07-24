a6aedf6a09c7e42ac5d2e9cc32f09fc652a43dc2
Reynah Sharon <reynah.sharon@cerence.com>
2020-07-24 10:45:46 -0400

    added parser logic for multiple PTT sessions

--- parser.py
+++ parser.py
@@ -2,42 +2,21 @@ import csv
 import re
 
 
-# function to open log file and perform parsing
-def reader(filename):
-    with open(filename) as f:
-        log = f.read()
-        regex_PTT = ".*beginSpeechFrame"
-
-        list_PTT = []
-        list_PTT.append(re.findall(regex_PTT, log)[-1])
-        ts_PTT = unique(list_PTT)  # finds the last final ASR result timestamp
-
-        regex_playString = "(.*nuance_prompter_IPrompter_playString IPrompter_instance='SDS_prompter' " \
-                           "IPrompt_instance='IPrompt_)(?!.*wav.*)"
-        list_Play = []
-        list_Play.append(re.findall(regex_playString, log)[-1])
-
-        ts_playString = unique(list_Play)  # picks only response log lines. Ignores toneup and tonedown
-
-        print 'Session start: ', ts_PTT, 'Session end: ', ts_playString
-        return latency(ts_PTT, ts_playString)
-
-
 # function to calculate latency
-def latency(ts_PTT, ts_playString):
-    return ts_playString - ts_PTT + 600
-
-
-# function to reverse a list of strings
-def rever(strings):
-    return [x[::-1] for x in strings]
+def latency(ts_PTT_list, ts_playString_list):
+    latency_list = []
+    for (ts_PTT, ts_playString) in zip(ts_PTT_list, ts_playString_list):
+        latency = ts_playString - ts_PTT + 600
+        latency_list.append(latency)
+    print latency_list
+    return latency_list
 
 
 # function to pick timestamp from log and convert it to integer
-def stripper(playstring_list):
+def stripper(timestamp_list):
     response_timestamp = 0
     regexp = "[^\n]\d\d\d\d\d\d\d\d\d\d"
-    for playstring in playstring_list:
+    for playstring in timestamp_list:
         stripped_list = re.findall(regexp, playstring)  # picks only timestamps from the log line
         for tss in stripped_list:
             response_timestamp = int(tss.replace('_', ''))
@@ -45,34 +24,37 @@ def stripper(playstring_list):
 
 
 # function to get unique values
-def unique(list1):
+def unique(timestamp_list):
     # insert the list to the set
-    list_set = set(list1)
-    unique_list = (list(list_set))  # removes duplicate playstring values from list
+    list_set = set(timestamp_list)
+    unique_list = (list(list_set))  # removes duplicate entries from list
     response_timestamp = stripper(unique_list)
     return response_timestamp
 
 
 # function to generate csv file
-def write_csv(latency):
-    with open('output.csv', 'w') as csvfile:
+def write_csv(latency_list):
+    min_latency = min(latency_list)
+    max_latency = max(latency_list)
+    average_latency = sum(latency_list) / len(latency_list)
+    attempts_count = len(latency_list)
+    utterance = 'Number 2'  # TODO: Enter correct command
+    with open('output.csv', 'wb') as csvfile:
         writer = csv.writer(csvfile)
-
-        header = ['Utterance', 'Latency(milliseconds)']
-
+        header = ['Utterance', 'No. of attempts', 'AVG(ms)', 'MIN(ms)', 'MAX(ms)']
         writer.writerow(header)
-
-        # writer.writerow(('Number 1', 2109))  # TODO: Enter correct command
-        # writer.writerow(('Number 1', 2702))  # TODO: Enter correct command
-        writer.writerow(('Number 2', 2526))  # TODO: Enter correct command
-        writer.writerow(('Number 2', latency))  # TODO: Enter correct command
+        writer.writerow(
+            (utterance, attempts_count, average_latency, min_latency, max_latency))
 
 
-def file_splitter(filename):
+# function to open log file and perform parsing
+def log_reader(filename):
     with open(filename) as f:
         log = f.read()
         block_regex = "EVNT GDM NAME=PTT hmi_active_screen=HMI_ACTIVE_NONE[\w\W]*?Scheduled event after flush: N"
         regex_PTT = ".*beginSpeechFrame"
+        regex_playString = "(.*nuance_prompter_IPrompter_playString IPrompter_instance='SDS_prompter' " \
+                           "IPrompt_instance='IPrompt_)(?!.*wav.*)"
         ts_PTT_list = []
         ts_playString_list = []
         result = re.findall(block_regex, log)
@@ -82,25 +64,18 @@ def file_splitter(filename):
             ts_PTT = unique(list_PTT)
             ts_PTT_list.append(ts_PTT)
             print 'ts_PTT: ', ts_PTT
-        for block in result:
-            regex_playString = "(.*nuance_prompter_IPrompter_playString IPrompter_instance='SDS_prompter' " \
-                               "IPrompt_instance='IPrompt_)(?!.*wav.*)"
             list_Play = []
             list_Play.append(re.findall(regex_playString, block)[-1])
-
             ts_playString = unique(list_Play)
             ts_playString_list.append(ts_playString)
             print 'ts_playString', ts_playString
 
     print ts_PTT_list
     print ts_playString_list
-    # print result
-    # print len(result)
+    latency_list = latency(ts_PTT_list, ts_playString_list)
+    write_csv(latency_list)
 
 
 # main function
 if __name__ == '__main__':
-    # response_timestamp = reader('log')
-    # print 'Latency: ', response_timestamp
-    # write_csv(response_timestamp)
-    file_splitter('log')
+    log_reader('log')
